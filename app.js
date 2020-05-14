ac9caf63c0c2901b90fb23b8ab06cd734c6a1424
Justin Brooks <justinbrooks@Justins-MacBook-Pro.local>
2020-05-14 14:10:44 -0500

    added alternating player js

--- app.js
+++ app.js
@@ -2,12 +2,51 @@ let cells = document.querySelectorAll(".row > div");
 
 console.log(cells);
 
+let player = "X";
+
 for (let i = 0; i < cells.length; i++) {
   cells[i].addEventListener("click", cellClicked);
 }
 
 function cellClicked() {
-  event.target.textContent = "X";
+  if (event.target.textContent === "") {
+    event.target.textContent = player;
+    if (player == "X") {
+      player = "O";
+    } else {
+      player = "X";
+    }
+  }
 }
 
+cells[0];
+cells[1];
+cells[2];
 
+cells[3];
+cells[4];
+cells[5];
+
+cells[6];
+cells[7];
+cells[8];
+
+cells[0];
+cells[3];
+cells[6];
+
+cells[1];
+cells[4];
+cells[7];
+
+cells[2];
+cells[5];
+cells[8];
+
+cells[0];
+cells[4];
+cells[8];
+
+cells[6];
+cells[4];
+cells[2];
