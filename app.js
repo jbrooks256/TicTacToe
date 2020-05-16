37a2977f60fc8c3c2257f742e925946d6f247d12
Justin Brooks <justinbrooks@Justins-MacBook-Pro.local>
2020-05-15 22:03:42 -0500

    changed win con txt

--- app.js
+++ app.js
@@ -29,89 +29,58 @@ function winCondition() {
       cells[1].textContent === cells[2].textContent &&
       cells[0].textContent != ""
     ) {
-      alert("gg2ez");
+      alert("gg 2ez");
     }
     if (
       cells[3].textContent === cells[4].textContent &&
       cells[4].textContent === cells[5].textContent &&
       cells[3].textContent != ""
     ) {
-      alert("gg2ez");
+      alert("gg 2ez");
     }
     if (
       cells[6].textContent === cells[7].textContent &&
       cells[7].textContent === cells[8].textContent &&
       cells[6].textContent != ""
     ) {
-      alert("gg2ez");
+      alert("gg 2ez");
     }
     if (
       cells[0].textContent === cells[3].textContent &&
       cells[3].textContent === cells[6].textContent &&
       cells[0].textContent != ""
     ) {
-      alert("gg2ez");
+      alert("gg 2ez");
     }
     if (
       cells[1].textContent === cells[4].textContent &&
       cells[4].textContent === cells[7].textContent &&
       cells[1].textContent != ""
     ) {
-      alert("gg2ez");
+      alert("gg 2ez");
     }
     if (
       cells[2].textContent === cells[5].textContent &&
       cells[5].textContent === cells[8].textContent &&
       cells[2].textContent != ""
     ) {
-      alert("gg2ez");
+      alert("gg 2ez");
     }
     if (
       cells[0].textContent === cells[4].textContent &&
       cells[4].textContent === cells[8].textContent &&
       cells[0].textContent != ""
     ) {
-      alert("gg2ez");
+      alert("gg 2ez");
     }
     if (
       cells[6].textContent === cells[4].textContent &&
       cells[4].textContent === cells[2].textContent &&
       cells[6].textContent != ""
     ) {
-      alert("gg2ez");
+      alert("gg 2ez");
     } else if (counter === 10) {
       alert("DRAW");
     }
   }
 }
-// cells[0];
-// cells[1];
-// cells[2];
-
-// cells[3];
-// cells[4];
-// cells[5];
-
-// cells[6];
-// cells[7];
-// cells[8];
-
-// cells[0];
-// cells[3];
-// cells[6];
-
-// cells[1];
-// cells[4];
-// cells[7];
-
-// cells[2];
-// cells[5];
-// cells[8];
-
-// cells[0];
-// cells[4];
-// cells[8];
-
-// cells[6];
-// cells[4];
-// cells[2];
